bb0e5d00355fd2a86025ebe6a70674a2d2ed3b9e
roman.chuvilev <roman.chuvilev@arkadium.com>
2025-03-27 14:17:22 +0400

    feat: Added favicons and some meta meanwhile ref: Main functionality

--- apps/alt-shift-app/src/components/Atoms/TypographyTitle/index.tsx
+++ apps/alt-shift-app/src/components/Atoms/TypographyTitle/index.tsx
@@ -1,14 +1,16 @@
+import React from "react";
 import clsx from "clsx";
 
 import styles from './index.module.css';
 
 type TProps = {
     level: 1 | 2 | 3 | 4 | 5 | 6;
+    children: React.ReactNode;
 }
 
-const Component = ({level}: TProps) => {
+const Component = ({level, children}: TProps) => {
     const tag = `h${level ?? 1}`;
-    const TitleTag = React.createElement(tag);
+    const TitleTag = (props: any) => React.createElement(tag, props);
     return (
         <TitleTag className={clsx(styles.Title, styles[tag])}>
             {children}
