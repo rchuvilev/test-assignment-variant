b80ad88c5acfa045274b9c1a5a0b7f17a06e7046
roman.chuvilev <roman.chuvilev@arkadium.com>
2025-03-30 17:28:00 +0400

    feat: Form alignments ref: Main functionality

--- apps/alt-shift-app/src/features/AiRequest/aiRequest.ts
+++ apps/alt-shift-app/src/features/AiRequest/aiRequest.ts
@@ -1,1 +1,21 @@
-export const aiRequest = async () => {};
+export const aiRequest = async ({onSuccess, onError, finalCb} : {
+    errorCb: (error: any) => void;
+    successCb: (result: any) => void;
+    finalCb: () => void;
+}) => {
+    const simulateLongerRequest = () => new Promise(resolve => setTimeout(() => resolve('Response'), 3000));
+    const loadAiResponse = async () => 'AAAAAA';
+    let result: any = null;
+    try {
+        result = await Promise.all([
+            loadAiResponse(), // goes first for [0] access
+            simulateLongerRequest(),
+        ]);
+        const response = result[0];
+        onSuccess(response);
+    } catch (error) {
+        onError(error);
+    } finally {
+        finalCb();
+    }
+};
